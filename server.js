1f019a26eef5303289efe146783537ea7ee82045
Ethan-Robertson <159573772+Ethan-Robertson@users.noreply.github.com>
2024-07-22 13:32:18 -0400

    Adjest Structure for Render

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
